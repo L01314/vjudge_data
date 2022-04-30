d3a1f7807fb3d8246e7960ee6ef40fa1b79c7f1b
L01314 <79087104+L01314@users.noreply.github.com>
2022-04-30 20:02:40 +0800

    Update T1155病人排队.cpp

--- T1155病人排队.cpp
+++ T1155病人排队.cpp
@@ -1,4 +1,30 @@
-#include<bits/stdc++.h>
+#include<stdio.h>
+int i,j,n; 
+struct node
+{
+	char  num[100];
+	int age;
+}s[100],k;
+int main()
+{
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+		scanf("%s %d",s[i].num,&s[i].age);
+	for(i=1;i<n;i++)
+		for(j=0;j<n-i;j++)
+	{
+		if(s[j+1].age>s[j].age&&s[j+1].age>=60)
+		{
+			k=s[j+1];s[j+1]=s[j];s[j]=k;
+		}
+	}
+		for(int i=0;i<n;i++)
+			printf("%s\n",s[i].num);
+}
+
+
+
+/*#include<bits/stdc++.h>
 using namespace std;
 int i,j,k,n;
 struct node
@@ -13,12 +39,13 @@ bool cmp(node a,node b)
 }
 int main()
 {
-    cin>>n;
+	cin>>n;
 	for(int i=0;i<n;i++)	cin>>s[i].id>>s[i].year;
 	sort(s,s+n,cmp);
 	
 	for(int i=0;i<n;i++)
 		cout<<s[i].id<<endl;
 	
-
+	
 }
+*/
